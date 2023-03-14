8cdfce8408fd6e001e831ed2e7da1f192f949ab3
Berika <berika.varol@bil.omu.edu.tr>
2023-03-14 16:52:25 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,37 +1,43 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-import cv2
+import random
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
 
-from tqdm import tqdm
+random.shuffle(test_data)
 
-#DATA = '/content/gdrive/My Drive/CNN_dataset_deneme/test_data'
+for sample in test_data[:10]:
+    X_test = []
+    y_test = []
 
-def plot_image(input, predictions_array):            
-    
-    
-    predictions_array = predictions_array[input]
-    plt.grid(False)
-    plt.xticks([])
-    plt.yticks([])
-    
-    
-    
-    kanser_riski= 100*np.max(predictions_array) 
-   
-    if kanser_riski>50:
-       sonuc=class_names[0]
-    else:
-       sonuc=class_names[1]
-  
-  
-    print("kanser riski: %", kanser_riski)
-    print("sonuç:",sonuc)
-    print("Görsel:")
-    sonuc_path = os.path.join(DATADIR_TEST_3)  
+for features,label in test_data:
+    X_test.append(features)
+    y_test.append(label)
 
-    sonuc_img_array= cv2.imread(os.path.join(sonuc_path,os.listdir(sonuc_path)[input]))
+y_test1 = np.array(y_test)
 
-    sonuc_new_array = cv2.resize(sonuc_img_array, (IMG_SIZE, IMG_SIZE))
-    plt.imshow(sonuc_new_array, cmap='gray')
-    plt.show()
+X_test= np.array(X_test).reshape(-1, IMG_SIZE, IMG_SIZE,3)
+model.evaluate(X_test, y_test1)
+prediction = model.predict(X_test)
+
+prediction= (prediction > 0.5)
+prediction = prediction.astype(int)
+confusion_matrix(y_test1, prediction)
+
+print(confusion_matrix(y_test1, prediction))
+print(classification_report(y_test1, prediction))
+print("Accuracy: {0}".format(accuracy_score(y_test1, prediction)))
+
+
+prediction = model.predict(X_test).ravel()
+fpr_keras, tpr_keras, thresholds_keras = roc_curve(y_test1, prediction)
+
+auc_keras = auc(fpr_keras, tpr_keras)
+
+plt.figure(1)
+plt.plot([0, 1], [0, 1], 'k--')
+plt.plot(fpr_keras, tpr_keras, label='AUC = {:.3f}'.format(auc_keras))
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Eğrisi')
+plt.legend(loc='best')
+plt.show()
