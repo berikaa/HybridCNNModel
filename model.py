8baae15cff4a080e2b9b4f632f16a65b774208e9
Berika <berika.varol@bil.omu.edu.tr>
2023-03-14 16:49:05 +0300

    Update model.py

--- model.py
+++ model.py
@@ -81,15 +81,3 @@ model.add(Dropout(0.5))
 
 model.add(Dense(1, activation='sigmoid'))
 
-tensorboard = tf.keras.callbacks.TensorBoard(log_dir="logs/fit/".format(NAME))
-
-model.compile(loss='mean_squared_error',optimizer='rmsprop',metrics=['accuracy'])
-
-model.summary()
-#(learning_rate=0.001, rho=0.9) #...
-model.fit(train_filenames, train_labels,
-          batch_size=32,
-          epochs=10,
-          verbose=1,
-          validation_data=(val_filenames, val_labels),
-          callbacks=[tensorboard])
